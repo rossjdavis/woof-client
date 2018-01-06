00bb279b8a9f36dbfd5a0f857e3af1334f3d00a8
Ross Davis <rossjdavis@gmail.com>
2018-01-06 11:00:29 -0500

    websockets resolved - use immutable

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import App from './App'
 
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import canineReducer from './reducers/canines'
-
 import { init as listen, emit } from './actions/websockets'
+import { List, Map } from 'immutable'
+
+const initialState = new Map().set('canines', new List())
 
 function startup() {
-  const store = createStore(canineReducer)
+  const middleware = [thunkMiddleware.withExtraArgument({ emit })]
+  const store = createStore(
+    canineReducer,
+    initialState,
+    applyMiddleware(...middleware)
+  )
 
   console.log(store.getState())
 
